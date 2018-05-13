9afca5db771be4fb3dc6dca7fcc29f21cf1a557a
Artur Sadykov <amsadykov@gmail.com>
2018-05-13 21:33:47 +0300

    Добавлена регистрация нового пользователя

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EFGetStarted.AspNetCore.ExistingDb.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace EFGetStarted.AspNetCore.ExistingDb.Controllers
+{
+    [Produces("application/json")]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost]
+        [Route("api/Account/Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User { Email = model.Email, UserName = model.Email };
+                // Добавление нового пользователя
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    // установка куки
+                    await _signInManager.SignInAsync(user, false);
+                    var msg = new
+                    {
+                        message = "Добавлен новый пользователь: " + user.UserName
+                    };
+                    return Ok(msg);
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    var errorMsg = new
+                    {
+                        message = "Пользователь не добавлен.",
+                        error = ModelState.Values.SelectMany(e => e.Errors.Select(er => er.ErrorMessage))
+                    };
+                    return Ok(errorMsg);
+                }
+            }
+            else
+            {
+                var errorMsg = new
+                {
+                    message = "Неверные входные данные.",
+                    error = ModelState.Values.SelectMany(e => e.Errors.Select(er => er.ErrorMessage))
+                };
+                return Ok(errorMsg);
+            }
+        }
+    }
+}
