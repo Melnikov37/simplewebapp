60e7e7d7e77634f6c61c0e84a9a71b75ca9d1e22
Artur Sadykov <sadykov@gapps.ispu.ru>
2018-05-15 15:31:38 +0300

    Изменения по миграции данных

---
+++ Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using EFGetStarted.AspNetCore.ExistingDb.Models;
+
+namespace Web
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<BloggingContext>
+    {
+        public BloggingContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<BloggingContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new BloggingContext(builder.Options);
+        }
+    }
+}
