18484227bb20196153cd0564db4abbbf8dd054c0
Artur Sadykov <amsadykov@gmail.com>
2018-05-17 22:46:51 +0300

    Добавлено заполнение БД тест. д-ми при инициализации

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFGetStarted.AspNetCore.ExistingDb.Models;
+
+namespace EFGetStarted.AspNetCore.ExistingDb.Data
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(BloggingContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Blog.Any())
+            {
+                return;
+            }
+
+            var blogs = new Blog[]
+            {
+                new Blog{Url="http://blogs.msdn.com/dotnet"},
+                new Blog{Url="http://blogs.msdn.com/webdev"},
+                new Blog{Url="http://blogs.msdn.com/visualstudio"}
+
+            };
+            foreach (Blog b in blogs)
+            {
+                context.Blog.Add(b);
+            }
+            context.SaveChanges();
+
+            var posts = new Post[]
+            {
+                new Post { BlogId=1,Content="Today, we are releasing the .NET Core May 2018 Update. This update includes .NET Core 2.1.200 SDK and ASP.NET Core 2.0.8. Security Microsoft is releasing this security advisory to provide information about a vulnerability in .NET Core and .NET native version 2.0.",Title=".NET Core May 2018 Update" },
+                new Post { BlogId=1,Content="Today, we are excited to announce that the first release candidate of EF Core 2.1 is available, alongside .NET Core 2.1 RC 1 and ASP.NET Core 2.1 RC 1, for broad testing, and now also for production use! Go live support EF Core 2.1 RC1 is a “go live” release, which means once you test…",Title="Announcing Entity Framework Core 2.1 RC 1" }
+            };
+            foreach (Post p in posts)
+            {
+                context.Post.Add(p);
+            }
+            context.SaveChanges();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using EFGetStarted.AspNetCore.ExistingDb.Models;
+using EFGetStarted.AspNetCore.ExistingDb.Data;
+using Microsoft.Extensions.DependencyInjection.Abstractions;
 
 namespace EFGetStarted.AspNetCore.ExistingDb
 {
@@ -14,12 +18,32 @@ namespace EFGetStarted.AspNetCore.ExistingDb
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<BloggingContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+
+
     }
+
 }
